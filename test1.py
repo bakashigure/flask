6ed120671b45ed180d355c76e596b3c0a6fd34c2
shigure <33803916+bakashigure@users.noreply.github.com>
2019-05-21 21:58:03 +0800

    test1

---
+++ test1.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__,static_url_path='',root_path='/home/ronny/flask')    
+@app.route('/')
+def index():
+    return app.send_static_file('pathetic.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8081,debug=True)
