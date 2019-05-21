0eea8a2b8dc6a28bdea944967e5f64e75551fb55
shigure <33803916+bakashigure@users.noreply.github.com>
2019-05-21 23:01:33 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,8 @@
 from flask import Flask
+import os
+t=os.getcwd()
 
-app = Flask(__name__,static_url_path='',root_path='~/Desktop/flask')    
+app = Flask(__name__,static_url_path='',root_path=t)    
 @app.route('/')
 def index():
     return app.send_static_file('pathetic.html')
